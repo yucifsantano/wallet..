8ad0e5a6ccbc39837506a5677a5d90c1dbd4a7f5
Yucif Santano <ysantano@ciericorp.com>
2022-02-05 11:34:53 -0600

    Primera pantalla terminada.

--- components/TransactionHistory.tsx
+++ components/TransactionHistory.tsx
@@ -31,6 +31,19 @@ const TransactionHistory = ({customContainerStyle, history}:{customContainerStyl
                 <Text style={{...FONTS.h3}}>{item.description}</Text>
                 <Text style={{color:COLORS.gray, ...FONTS.body4}}>{item.date}</Text>
             </View>
+            <View style={{flexDirection:'row', height:'100%', alignItems:'center'}}>
+                <Text style={{ color: item.type == "B" ? COLORS.green : COLORS.black, ...FONTS.h3 }}>
+                    {item.amount} {item.currency}
+                </Text>
+                <Image
+                    source={icons.right_arrow}
+                    style={{
+                        width:20,
+                        height:20,
+                        tintColor:COLORS.gray,
+                    }}
+                />
+            </View>
         </TouchableOpacity>
     )
     
